f618a5c33147a3b38241d53dc5d7fee404bffbb0
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-23 16:07:34 +0100

    java 11 collection to Array

--- Java-11/src/main/java/Demo.java
+++ Java-11/src/main/java/Demo.java
@@ -1,16 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Demo {
     public static void main(String[] args) {
 
-     }
+    }
 
-      public static List<String> newStringMethods (){
+    public static List<String> newStringMethods() {
         String multilineString = "All Algeria \n \n Java \n Community.";
         return multilineString.lines()
                 .filter(line -> !line.isBlank())
                 .map(String::strip)
                 .collect(Collectors.toList());
     }
+
+    public static String newFileMethods() {
+        String home = System.getProperty("user.home");
+        Path filePath = Paths.get(home, "test.txt");
+        try {
+            Files.writeString(filePath, "Hello World !!");
+            return Files.readString(filePath);
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    public static String[] collectiontoArray() {
+        var sampleList = Arrays.asList("Algiers", "Setif");
+        return sampleList.toArray(String[]::new);
+    }
+
 }
--- Java-11/src/test/java/DemoTest.java
+++ Java-11/src/test/java/DemoTest.java
@@ -2,12 +2,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DemoTest {
 
     @Test
-    void newStringMethodsTest() {
-         assertTrue(Demo.newStringMethods().equals(Arrays.asList("All Algeria", "Java", "Community.")));
+    public void newStringMethodsTest() {
+        assertTrue(Demo.newStringMethods().equals(Arrays.asList("All Algeria", "Java", "Community.")));
+    }
+
+    @Test
+    public void newFileMethodsTest() {
+        assertTrue(Demo.newFileMethods().equals("Hello World !!"));
+    }
+
+    @Test
+    public void collectiontoArrayTest() {
+        assertTrue(Arrays.equals(Demo.collectiontoArray(), new String[]{"Algiers", "Setif"}));
     }
 }
+
